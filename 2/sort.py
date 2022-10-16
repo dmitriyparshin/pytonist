25dc75b57086c77360b7c5d31a8a35c6a230b12b
dmitriyparsin <dmitriyparshin09@gmail.com>
2022-10-16 20:14:00 +0300

    1

---
+++ 2/sort.py
@@ -0,0 +1,15 @@
+list = [1, 4, 2, 9, 5, 7, -7, 0, -45]
+
+
+def sortNum(list):
+    global len
+    len = len(list)
+    for i in range(len):
+        for l in range(0, len - i - 1):
+            if list[l] > list[l + 1]:
+                temp = list[l]
+                list[l] = list[l + 1]
+                list[l + 1] = temp
+
+sortNum(list)
+print(list)
